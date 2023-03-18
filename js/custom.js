fadbaa37073974380de3f8fbfcca39e4fd71dfc7
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2023-03-18 23:47:26 +0600

    single product edited

--- js/custom.js
+++ js/custom.js
@@ -236,7 +236,7 @@ jQuery(function ($) {
     adaptiveHeight: true,
     slidesToScroll: 1,
     rtl: false,
-    // autoplay: true,
+    autoplay: false,
   });
 
   /* ----------------------------------------------------------- */
@@ -371,6 +371,41 @@ jQuery(function ($) {
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  jQuery(".aa-related-item-slider-custom").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,    
     responsive: [
       {
         breakpoint: 1024,
